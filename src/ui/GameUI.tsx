b250db050a259f5827b1ca8614ffc667892fe8a3
Jose D <joserafael161@hotmail.com>
2025-11-15 01:23:40 -0500

    Add more UI structure

--- src/ui/GameUI.tsx
+++ src/ui/GameUI.tsx
@@ -3,15 +3,25 @@ export const GameUI = () => {
         <>
             <div className="main-wrapper">
                 <div className="game-board">
-                    <div className="game-board__options-menu">Menu</div>
-                    <div className="game-board__trade-controls">Trade controls</div>
-                    <div className="game-board__card-holder">Card holder</div>
+                    <div className="game-board__top">
+                        <div className="game-board__options-menu">
+                            <button className="game-board__options-button">Options</button>
+                            <button className="game-board__options-button">Info</button>
+                        </div>
+                        <div className="game-board__trade-popup">
+                            <div className="game-board__trade-popup-top generic-box">Trade top</div>
+                            <div className="game-board__trade-popup-content generic-box">Trade info</div>
+                        </div>
+                    </div>
+                    <div className="game-board__bottom">
+                        <div className="game-board__card-holder generic-box">Card holder</div>
+                    </div>
                 </div>
                 <div className="game-controls">
-                    <div className="game-board__generic-box game-controls__log">Log</div>
-                    <div className="game-board__generic-box game-controls__chat">Chat</div>
-                    <div className="game-board__generic-box game-controls__bank">Bank</div>
-                    <div className="game-board__generic-box game-controls__players">Players</div>
+                    <div className="game-controls__log generic-box">Log</div>
+                    <div className="game-controls__chat generic-box">Chat</div>
+                    <div className="game-controls__bank generic-box">Bank</div>
+                    <div className="game-controls__players generic-box">Players</div>
                 </div>
             </div>
             <div className="game"></div>
