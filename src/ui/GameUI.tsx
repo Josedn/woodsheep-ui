e1f6781dd68a4b4d175c194b40a32bc180fa815b
Jose D <joserafael161@hotmail.com>
2025-12-16 02:01:48 -0500

    Add log

--- src/ui/GameUI.tsx
+++ src/ui/GameUI.tsx
@@ -6,6 +6,15 @@ import cardOre from "../assets/ui/card_ore.117f64dab28e1c987958.svg";
 import cardGrain from "../assets/ui/card_grain.09c9d82146a64bce69b5.svg";
 import cardDevelopment from "../assets/ui/card_devcardback.92569a1abd04a8c1c17e.svg";
 import badgeBackgroundBlue from "../assets/ui/button_badge_background_blue.2e754ec21f9c79da6267.svg";
+import iconBot from "../assets/ui/icon_bot.fe8fdd5cc98ae77d7774.svg";
+import iconPlayer from "../assets/ui/icon_player_loggedin.88be0a3c581efb9f2d3a.svg";
+import iconTrophy from "../assets/ui/icon_trophy.bc5c68a7464f0462721d.svg";
+import dice1 from "../assets/ui/dice_1.f5a1a69c3529b5b5ffc5.svg";
+import dice2 from "../assets/ui/dice_2.859c1a230cf0ab52f238.svg";
+import dice3 from "../assets/ui/dice_3.353e115f936308bb6256.svg";
+import dice4 from "../assets/ui/dice_4.351f1ed668f38d45da30.svg";
+import dice5 from "../assets/ui/dice_5.5eea2c3e3b85be8190bd.svg";
+import dice6 from "../assets/ui/dice_6.aea83e2b0e712f5f1fab.svg";
 
 const generateCardStack = (count: number, imgSrc: string) => {
     const lastItemClassName = "game-controls__card-container" + (count == 0 ? " game-controls__card-container--empty" : "");
@@ -54,7 +63,169 @@ const generateLog = () => {
             <div className="game-controls__log-container">
                 <div className="game-controls__log-scroller">
                     <div className="game-controls__log-item">
-                        <div className="game-feed-message">Message</div>
+                        <div className="game-feed-message__container">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconBot}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <span className="game-feed-message__content--bold game-feed-message__content--blue">Lissi</span> bought <img className="game-feed-message__content-image" src={cardDevelopment}></img>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconBot}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <span className="game-feed-message__content--bold game-feed-message__content--red">Ester</span> got <img className="game-feed-message__content-image" src={cardGrain}></img>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <span className="game-feed-message__content">
+                                <hr />
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconBot}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <span className="game-feed-message__content--bold game-feed-message__content--green">Joost</span> rolled <img className="game-feed-message__content-image" src={dice3}></img>{" "}
+                                <img className="game-feed-message__content-image" src={dice5}></img>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconPlayer}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <span className="game-feed-message__content--bold game-feed-message__content--orange">Bold</span> got <img className="game-feed-message__content-image" src={cardLumber}></img>{" "}
+                                <img className="game-feed-message__content-image" src={cardOre}></img>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconBot}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <span className="game-feed-message__content--bold game-feed-message__content--green">Joost</span> rolled <img className="game-feed-message__content-image" src={dice3}></img>{" "}
+                                <img className="game-feed-message__content-image" src={dice5}></img>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconPlayer}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <span className="game-feed-message__content--bold game-feed-message__content--orange">Bold</span> got <img className="game-feed-message__content-image" src={cardLumber}></img>{" "}
+                                <img className="game-feed-message__content-image" src={cardOre}></img>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconBot}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <span className="game-feed-message__content--bold game-feed-message__content--blue">Lissi</span> bought <img className="game-feed-message__content-image" src={cardDevelopment}></img>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconBot}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <span className="game-feed-message__content--bold game-feed-message__content--red">Ester</span> got <img className="game-feed-message__content-image" src={cardGrain}></img>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <span className="game-feed-message__content">
+                                <hr />
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconBot}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <span className="game-feed-message__content--bold game-feed-message__content--green">Joost</span> rolled <img className="game-feed-message__content-image" src={dice3}></img>{" "}
+                                <img className="game-feed-message__content-image" src={dice5}></img>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconPlayer}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <span className="game-feed-message__content--bold game-feed-message__content--orange">Bold</span> got <img className="game-feed-message__content-image" src={cardLumber}></img>{" "}
+                                <img className="game-feed-message__content-image" src={cardOre}></img>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconBot}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <span className="game-feed-message__content--bold game-feed-message__content--blue">Lissi</span> bought <img className="game-feed-message__content-image" src={cardDevelopment}></img>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconBot}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <span className="game-feed-message__content--bold game-feed-message__content--red">Ester</span> got <img className="game-feed-message__content-image" src={cardGrain}></img>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className="game-controls__log-item">
+                        <div className="game-feed-message__container game-feed-message__container--centered">
+                            <div className="game-feed-message__icon">
+                                <img className="game-feed-message__image" src={iconBot}></img>
+                            </div>
+                            <span className="game-feed-message__content">
+                                <img className="game-feed-message__content-image" src={iconTrophy}></img> <span className="game-feed-message__content--bold game-feed-message__content--red">Ester</span> won the game!{" "}
+                                <img className="game-feed-message__content-image" src={iconTrophy}></img>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
